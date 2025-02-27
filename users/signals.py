8a0debeace50cb738fecfc503c5af89501b08cd8
mmahmudul7 <hasanitgroup@gmail.com>
2025-02-27 15:26:15 +0600

    Dynamic profile page

--- users/signals.py
+++ users/signals.py
@@ -1,10 +1,13 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save, m2m_changed
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import Group
 from django.contrib.auth.tokens import default_token_generator
 from django.conf import settings
 from django.core.mail import send_mail
 from events.models import Event, UserProfile
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 @receiver(post_save, sender=User)
 def send_activation_email(sender, instance, created, **kwargs):
